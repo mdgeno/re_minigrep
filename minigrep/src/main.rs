290246e39da6348bb0a4d6b911f10342e2af1853
mdgeno <manuelgenomer@gmail.com>
2026-06-02 22:50:00 +0800

    added minigrep folder

---
+++ minigrep/src/main.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::fs;
+use std::process;
+
+fn main(){
+
+	let arguments: Vec<String> = env::args().collect();
+
+	let config_args = match Config::build(&arguments){
+				Ok(val) => val,
+				Err(e) => {println!("Problem parsing arguments: {e}");
+					  process::exit(1);}
+			  };
+
+	println!("Searching for {}", config_args.query);
+	println!("In file {}", config_args.file_path);
+
+	let contents = fs::read_to_string(config_args.file_path).expect("error reading file");
+
+	println!("{contents}");
+
+}
+
+struct Config{
+	query: String,
+	file_path: String
+}
+
+impl Config{
+	fn build(args: &[String]) -> Result<Config, &'static str>{
+		if args.len()<3{
+			return Err("not enough arguments")
+		}
+
+		let query = args[1].clone();
+		let file_path = args[2].clone();
+
+		Ok(Config{ query: query, file_path: file_path}) 
+	}
+}
+
