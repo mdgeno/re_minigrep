9be2af4fa3667a9dea0142d1b818edb5f918969a
mdgeno <manuelgenomer@gmail.com>
2026-06-10 13:41:12 +0800

    refactored the search function to return an empty vector instead of unimplemented, for TDD purposes

--- minigrep_lib/src/lib.rs
+++ minigrep_lib/src/lib.rs
@@ -3,7 +3,7 @@ pub fn add(left: u64, right: u64) -> u64 {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
-	unimplemented!();
+	vec![]
 }
 
 #[cfg(test)]
@@ -15,4 +15,15 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+	#[test]
+	fn once(){
+		let query = "duct";	
+		let contents ="/
+Rust:
+safe, fast, productive.
+Pick three";
+
+	assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+	}
 }
